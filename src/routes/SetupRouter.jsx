e28edac942b0e4e3d84c6b3e73ad75eab3465c56
daffadmr <daffaardhika@gmail.com>
2022-11-06 23:30:52 +0700

    development: App setup router

---
+++ src/routes/SetupRouter.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Layout from "../components/Layout";
+import Dashboard from "../pages/Dashboard";
+import DiaryDetail from "../pages/DiaryDetail";
+import CreateDiary from "../pages/CreateDiary";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import PrivateRoute from "./PrivateRoute";
+import ProtectedRoute from "./ProtectedRoute";
+import EditDiary from "../pages/EditDiary";
+import ScrollToTop from "../components/ScrollToTop";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const SetupRouters = () => {
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="/" element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="dashboard/diari/:id" element={<DiaryDetail />} />
+          <Route path="dashboard/create" element={<CreateDiary />} />
+          <Route path="dashboard/edit/:id" element={<EditDiary />} />
+        </Route>
+        <Route path="/" element={<ProtectedRoute />}>
+          <Route path="/login" element={<Login />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default SetupRouters;
