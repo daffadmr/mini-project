1b172cf9101ec3cc53fb3cb0912802f935cae6a5
daffadmr <daffaardhika@gmail.com>
2022-11-06 23:31:11 +0700

    development: Add setup router in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import "./App.css";
 import SetupRouter from "./routes/SetupRouter";
-import { Helmet } from "react-helmet";
 
 function App() {
   return (
-    <div className="font-inter">
-      <SetupRouter>
-        <Helmet>
-          <title>Nested Title</title>
-          <meta name="description" content="Nested component" />
-        </Helmet>
-      </SetupRouter>
+    <div className="font-inter overflow-x-hidden">
+        <SetupRouter />
     </div>
   );
 }
