2c962f0117274c93cb809fdd557c02723642bc98
Daffa Damar <daffaardhika@gmail.com>
2023-05-26 13:08:24 +0700

    development: Change data type of subs

--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -24,3 +24,12 @@ subscription UserDiary($user_id: uuid) {
   }
 }
 `
+export const DIARY_USER_SUBS_COUNT = gql `
+subscription MySubscription($user_id: uuid) {
+  diari_aggregate(where: {user_id: {_eq: $user_id}}) {
+    aggregate {
+      count
+    }
+  }
+}
+`
